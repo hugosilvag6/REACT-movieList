280d65c6df30dc44d7ae37a9879258a672100524
hugosilvag6 <hugosilvag6@hotmail.com>
2022-04-30 21:04:51 -0300

    update

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,36 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+import { GenreResponseProps } from '../App';
+import { MovieProps } from '../App';
+import '../styles/content.scss';
+
+type Props = {
+  selected: GenreResponseProps,
+  movies: MovieProps[]
+}
+
+export function Content({selected, movies}: Props) {
+    // Complete aqui
+    return (
+        <div className="container">
+            <header>
+                <span className="category">
+                    Categoria:<span> {selected.title}</span>
+                </span>
+            </header>
+
+            <main>
+                <div className="movies-list">
+                    {movies.map(movie => (
+                        <MovieCard
+                            key={movie.imdbID}
+                            title={movie.Title}
+                            poster={movie.Poster}
+                            runtime={movie.Runtime}
+                            rating={movie.Ratings[0].Value}
+                        />
+                    ))}
+                </div>
+            </main>
+        </div>
+    );
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+import { GenreResponseProps } from '../App';
+import { MovieProps } from '../App';
+import '../styles/sidebar.scss';
+
+type Props = {
+  genres: GenreResponseProps[],
+  selectedGenreId: number,
+  changeId: (id: number) => void
+}
+
+export function SideBar({genres, selectedGenreId, changeId}: Props) {
+    // Complete aqui
+    return (
+        <nav className="sidebar">
+            <span>
+                Watch<p>Me</p>
+            </span>
+
+            <div className="buttons-container">
+                {genres.map((genre) => (
+                    <Button
+                        key={String(genre.id)}
+                        title={genre.title}
+                        iconName={genre.name}
+                        onClick={() => changeId(genre.id)}
+                        selected={selectedGenreId === genre.id}
+                    />
+                ))}
+            </div>
+        </nav>
+    );
 }
